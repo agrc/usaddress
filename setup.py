23a6475ea2518d9351c273a437e016b8dda989cb
stdavis <stdavis@utah.gov>
2021-01-04 14:07:23 -0700

    chore: v0.6.1 version bump and change log

--- setup.py
+++ setup.py
@@ -8,7 +8,7 @@ except ImportError:
     )
 
 setup(
-    version='0.6.0',
+    version='0.6.1',
     url='https://github.com/agrc/usaddress',
     description='Parse US (optimized for Utah by AGRC) addresses using conditional random fields',
     name='agrc-usaddress',
